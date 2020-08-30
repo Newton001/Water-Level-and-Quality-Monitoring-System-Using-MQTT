ce3dc233298ef1b8079e49178b2c033ecf845d7c
Newton001 <45996137+Newton001@users.noreply.github.com>
2020-08-30 14:47:23 +0300

    Create Water_level_and_Quality_Monitoring_System.ino
    Code is inclusive only of pump, Ultrasonic sensor and data transmission to Blynk App.

---
+++ Water_level_and_Quality_Monitoring_System.ino
@@ -0,0 +1,100 @@
+/*
+********************************************
+Authors: Newton Kelvin
+         Salome Chepchirchir
+PROJECT: Water Level and Quality Monitoring System Using MQTT
+********************************************
+*/
+#define TRIGGER 4
+#define ECHO    5
+#define PUMP 8
+
+// NodeMCU Pin D1 > TRIGGER | Pin D2 > ECHO
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "xxxxxxxxxxxxx";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "xxxxx";
+char pass[] = "xxxxxxxxxxx";
+
+
+void setup() {
+  
+  Serial.begin (9600);
+  Blynk.begin(auth, ssid, pass);
+  pinMode(TRIGGER, OUTPUT);
+  pinMode(ECHO, INPUT);
+  pinMode(BUILTIN_LED, OUTPUT);
+  pinMode(PUMP, OUTPUT);
+}
+
+void loop() {
+  
+  long duration, distance;
+  digitalWrite(TRIGGER, LOW);  
+  delayMicroseconds(2); 
+  
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(10); 
+  
+  digitalWrite(TRIGGER, LOW);
+  duration = pulseIn(ECHO, HIGH);
+  distance = (duration/2) / 29.1;
+
+  //Sending Water Level to Blynk App for Observation
+
+   if (distance <= 150) {
+    Blynk.virtualWrite(V0, 255);
+}
+  else {
+    Blynk.virtualWrite(V0, 0);
+  }
+
+ if (distance <= 100) {
+    Blynk.virtualWrite(V1, 255);
+}
+  else {
+    Blynk.virtualWrite(V1, 0);
+  }
+
+   if (distance <= 80) {
+    Blynk.virtualWrite(V2, 255);
+}
+  else {
+    Blynk.virtualWrite(V2, 0);
+  }
+
+   if (distance <= 40) {
+    Blynk.virtualWrite(V3, 255);
+}
+  else {
+    Blynk.virtualWrite(V3, 0);
+  }
+
+   if (distance <= 20) {
+    Blynk.virtualWrite(V4, 255);
+}
+  else {
+    Blynk.virtualWrite(V4, 0);
+  }
+  /* PUMP ACTIVATION AND SWITCHING */
+  if (distance < 100){
+    digitalWrite(PUMP, HIGH);
+  }
+  else if (distance > 100){
+    digitalWrite(PUMP);
+  }  
+  
+  Serial.print(distance);
+  Serial.println("Centimeter:");
+  Blynk.virtualWrite(V5, distance);
+  delay(200);
+  Blynk.run();
+}
