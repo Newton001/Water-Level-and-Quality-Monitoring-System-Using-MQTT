95d48280682606ce4781d8b38dbd0e755886168c
Newton001 <Newtonkelvin75@gmail.com>
2020-10-02 08:34:46 +0000

    Collection of Water State Paarameters

--- Water_level_and_Quality_Monitoring_System.ino
+++ Water_level_and_Quality_Monitoring_System.ino
@@ -5,9 +5,21 @@ Authors: Newton Kelvin
 PROJECT: Water Level and Quality Monitoring System Using MQTT
 ********************************************
 */
+#include <SoftwareSerial.h>                           //we have to include the SoftwareSerial library, or else we can't use it
 #define TRIGGER 4
 #define ECHO    5
 #define PUMP 8
+#define sensorValue A0
+#define rx 2                                          //define what pin rx is going to be
+#define tx 3                                          //define what pin tx is going to be
+#define SensorPin A1
+
+SoftwareSerial myserial(rx, tx);                      //define how the soft serial port is going to work
+unsigned long int avgValue;  //Store the average value of the sensor feedback
+float b;
+int buf[10],temp;
+
+
 
 // NodeMCU Pin D1 > TRIGGER | Pin D2 > ECHO
 
@@ -37,6 +49,10 @@ void setup() {
 
 void loop() {
   
+  
+}
+
+void waterLevel(){
   long duration, distance;
   digitalWrite(TRIGGER, LOW);  
   delayMicroseconds(2); 
@@ -97,4 +113,48 @@ void loop() {
   Blynk.virtualWrite(V5, distance);
   delay(200);
   Blynk.run();
+}
+
+//Measure Water Turbidity
+
+void turbidity(){
+  int sensorValue = analogRead(A0);// read the input on analog pin 0:
+  float voltage = sensorValue * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  Serial.println(voltage); // print out the value you read:
+  delay(500);
+}
+
+//Measure and Average the Water ph Values
+
+void sensorph()
+{
+  for(int i=0;i<10;i++)       //Get 10 sample value from the sensor for smooth the value
+  { 
+    buf[i]=analogRead(SensorPin);
+    delay(10);
+  }
+  for(int i=0;i<9;i++)        //sort the analog from small to large
+  {
+    for(int j=i+1;j<10;j++)
+    {
+      if(buf[i]>buf[j])
+      {
+        temp=buf[i];
+        buf[i]=buf[j];
+        buf[j]=temp;
+      }
+    }
+  }
+  avgValue=0;
+  for(int i=2;i<8;i++)                      //take the average value of 6 center sample
+    avgValue+=buf[i];
+  float phValue=(float)avgValue*5.0/1024/6; //convert the analog into millivolt
+  phValue=3.5*phValue;                      //convert the millivolt into pH value
+  Serial.print("    pH:");  
+  Serial.print(phValue,2);
+  Serial.println(" ");
+  digitalWrite(13, HIGH);       
+  delay(800);
+  digitalWrite(13, LOW); 
+ 
 }
